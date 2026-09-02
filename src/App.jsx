d94a7bbda662b8e17b303192bfcca5d83f3706d0
kanku-io <jesuskanku.za@gmail.com>
2026-09-02 14:56:37 +0200

    Git workflows updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar'
-import Homepage from './homepage/homepage'
+import Homepage from './homepage/Homepage'
 
 const App = () => {
   return(
-    <BrowserRouter>
-    <NavBar/>
-
-    <Routes>
-
-      <Route path='/Pawned' element={<Homepage/>}></Route>
-
-    </Routes>
-    
-    </BrowserRouter>
-    
-    
+    <BrowserRouter basename="/Pawned">
+  <NavBar/>
+  <Routes>
+    <Route path='/' element={<Homepage/>}></Route>
+  </Routes>
+</BrowserRouter>
   )
 }
 
